02563132af0fef7f44244286bf7553e24835da78
chandusai <chandumovva4@gmail.com>
2020-12-07 21:27:13 +1300

    added es6 features

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 // let and var key words
 
-function sayHello(){
+function sayHell(){
  for(let i=0;i<10;i++){
   console.log(i);
  }
 console.log(i);
 }
-sayHello()
+sayHell()
+
+
+
+// By using the let keyword "i" can be accessible inside the "for code " block. 
+
+// But where as when we are using the var key word " i" can be accessible entire function.
+// When we are declaring the variables with "var" keyword are scoped to the function.
+// Varibles which are declared with let keyword are scoped with block in which they are defined.
+
+function sayHello(){
+    for(var i=0;i<6;i++){
+     console.log(i);
+    }
+   console.log(i);
+   }
+   sayHello()
